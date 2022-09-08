52eea6e242ff3dbea39209ebc1d2326ff02d67c8
UnicornTowa <90133563+UnicornTowa@users.noreply.github.com>
2022-09-09 00:46:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "facerec.h"
+using namespace std;
+
+
+int main(){
+    int nop = 500;
+    int people = 15;
+    int s = 8;
+    int n = 2;
+
+    auto pixels = choose_pixels(nop);
+
+    auto base = make_base(people, pixels, nop);
+
+    int* res = new int[people];
+    auto target = extract_vector_and_delete_matrix(make_arr(s, n), pixels, nop);
+    for (int i = 0; i < people; i++){
+        res[i] = calculate_difference(base[i], target, nop);
+    }
+    cout << "RESULT" << endl;
+
+    for (int i = 0; i < people; i++) {
+        cout << res[i] << endl;
+        delete [] base[i];
+    }
+    delete [] base;
+    delete [] res;
+    delete [] target;
+    delete [] pixels;
+    return 0;
+}
