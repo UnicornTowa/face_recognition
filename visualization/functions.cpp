771964c1496ea0a15a5ae883508a7bf4667b2b42
UnicornTowa <90133563+UnicornTowa@users.noreply.github.com>
2022-11-03 11:05:30 +0300

    Add files via upload

---
+++ visualization/functions.cpp
@@ -0,0 +1,249 @@
+#include "header.h"
+
+
+///Common
+
+long** picture_to_matrix_long(int s, int n)
+ {
+    auto ans = new long*[112];
+    auto int_ans = picture_to_matrix_int(s, n);
+    for (int i = 0; i < 112; i++){
+        auto temp = new long[92]();
+        for(int j = 0; j < 92; j++){
+            temp[j] = long(int_ans[i][j]);
+        }
+        ans[i] = temp;
+    }
+    delete [] int_ans;
+    return ans;
+}
+
+int** picture_to_matrix_int(int s, int n)
+{
+    string name = "database/s" + to_string(s) + "/" + to_string(n) + ".pgm";
+
+    int row = 0, col = 0, num_of_rows = 0, num_of_cols = 0;
+    stringstream ss;
+    ifstream infile(name, ios::binary);
+    string inputLine;
+
+    getline(infile,inputLine); // read the first line : P5
+    if(inputLine != "P5") cerr << "Version error" << endl;
+    //cout << "Version : " << inputLine << endl;
+
+    ss << infile.rdbuf(); //read the third line : width and height
+    ss >> num_of_cols >> num_of_rows;
+    //cout << num_of_cols << " columns and " << num_of_rows << " rows" << endl;
+
+    int max_val; //maximum intensity value : 255
+    ss >> max_val;
+    //cout<<max_val;
+
+    unsigned char pixel;
+
+    int **pixel_value = new int*[num_of_rows];
+    for(int i = 0; i < num_of_rows; ++i) {
+        pixel_value[i] = new int[num_of_cols];
+    }
+
+
+    int **integral = new int*[num_of_rows];
+    for(int i = 0; i < num_of_rows; ++i) {
+        integral[i] = new int[num_of_cols];
+    }
+
+
+    for (row = 0; row < num_of_rows; row++){ //record the pixel values
+        for (col = 0; col < num_of_cols; col++){
+            ss >> pixel;
+            pixel_value[row][col]= pixel;
+        }
+    }
+
+
+    integral[0][0]=pixel_value[0][0];
+    for(int i=1; i<num_of_cols;i++){ //compute integral image
+        integral[0][i]=integral[0][i-1]+pixel_value[0][i];
+    }
+    for (int i=1;i<num_of_rows; i++){
+        integral[i][0]=integral[i-1][0]+pixel_value[i][0];
+    }
+    for (int i = 1; i < num_of_rows; i++){
+        for (int j = 1; j < num_of_cols; j++){
+            integral[i][j] = integral[i - 1 ][j] + integral [i][j - 1] - integral[i - 1] [j - 1] + pixel_value[i][j];
+        }
+    }
+    //cout << "\nval " << pixel_value[0][1] << endl;
+    infile.close();
+
+
+    /*for(int i = 0; i < num_of_rows; ++i) {
+        delete [] pixel_value[i];
+    }
+    delete [] pixel_value;*/
+
+    for(int i = 0; i < num_of_rows; ++i) {
+        delete [] integral[i];
+    }
+    delete [] integral;
+
+    return pixel_value;
+}
+
+float get_acc(int mistakes, int people, int pb){
+    float acc = round(10000*(static_cast<float>((10 - pb)*people - mistakes)/static_cast<float>((10 - pb)*people)))/100;
+    //float acc = round(100 * ((10 - pb) * people - static_cast<float>(mistakes)) / (10 - pb) / people * 100) / 100;
+    return acc;
+}
+wstring get_acc(int count, int mistakes){
+    return wstring(to_wstring(round(10000*(static_cast<float>(count - mistakes))/static_cast<float>(count))/100), 0, 5);
+}
+
+/// Hist
+
+int get_weight(int h, int w){
+    if ((h == 1 or h == 2) and (w == 1 or w == 2)) // mid 4 sectors
+        return 1;
+    else if ((h == 0 or h == 3) and (w == 0 or w == 3)) // 4 corner sectors
+        return 4;
+    else if (h == 0) // upper-mid sectors
+        return 4;
+    else if (h == 3) // lower-mid sectors
+        return 3;
+    else return 1; // side-mid sectors
+}
+
+long * return_hist_v2(long **picture, int prec){
+
+    long* result_v2 = new long[256*16/prec]();
+    for(int w = 0; w < 4; w++){
+        for (int h = 0; h < 4; h++){
+            int correction = (4 * h + w) * 256 / prec;
+            int current_weight = get_weight(h, w);
+            for(int i = h * 28; i < (h + 1) * 28; i++){
+                for(int j = w * 23; j < (w + 1) * 23; j++){
+                    result_v2[picture[i][j] / (prec) + correction] += current_weight;
+                }
+            }
+        }
+    }
+    delete_matrix(picture);
+    return result_v2;
+}
+
+long ** make_multibase_hist(int people, int prec, int n1, int n2){
+    long** base = new long*[people*2];
+    for(int i = 0; i < people; i++){
+        base[i] = return_hist_v2(picture_to_matrix_long(i + 1, n1), prec);
+    }
+    for(int i = 0; i < people; i++){
+        base[i + people] = return_hist_v2(picture_to_matrix_long(i + 1, n2), prec);
+    }
+    return base;
+}
+
+long* get_result_hist(long** base, int n, int s, int people, int prec){
+    long* cur = return_hist_v2(picture_to_matrix_long(s, n), prec);
+    long* result = new long[people];
+    auto len = 16*256/prec;
+    for(int i = 0; i < people; i++){
+        result[i] = calculate_difference(cur, base[i], len);
+    }
+    delete [] cur;
+    return result;
+}
+
+/// Pixels
+
+pair<int, int>* choose_pixels(int num) {
+    auto* arr = new pair<int, int>[num];
+    for (int i = 0; i < num; i++) {
+        pair<int, int> p;
+        p.first = rand()%112;
+        p.second = rand()%92;
+        arr[i] = p;
+    }
+    return arr;
+}
+
+int* extract_vector_of_chosen_pixels(int** matrix, pair<int, int>* pixels, int len) {
+    int* vec = new int[len];
+    for (int i = 0; i < len; i++) {
+        vec [i] = matrix[pixels[i].first][pixels[i].second];
+    }
+    delete_matrix(matrix);
+    return vec;
+}
+
+int** make_multibase_pixels(int people, pair<int, int>*pixels, int nop, int n1, int n2) {
+    int** base = new int*[2*people];
+    for (int i = 0; i < people; i++){
+        base[i] = extract_vector_of_chosen_pixels(picture_to_matrix_int(i + 1, n1), pixels, nop);
+    }
+    for (int i = 0; i < people; i++){
+        base[i + people] = extract_vector_of_chosen_pixels(picture_to_matrix_int(i + 1, n2), pixels, nop);
+    }
+    return base;
+}
+
+int* get_result_pixels (int** base, pair<int,int>*pixels, int people, int nop, int s, int n) {
+    auto target = extract_vector_of_chosen_pixels(picture_to_matrix_int(s, n), pixels, nop);
+    int* res = new int[people]();
+    for (int i = 0; i < people; i++){
+        res[i] = calculate_difference(base[i], target, nop);
+    }
+    delete [] target;
+    return res;
+}
+
+/// Compress
+
+int*** make_multibase_compress(int people, int n1, int n2) {
+    int*** base_matrix = new int**[2*people];
+    for(int i = 0; i < people; i++){
+        base_matrix[i] = compress(picture_to_matrix_int(i + 1, n1));
+    }
+    for(int i = 0; i < people; i++){
+        base_matrix[i + people] = compress(picture_to_matrix_int(i + 1, n2));
+    }
+    return base_matrix;
+}
+
+int** compress(int** matrix) {
+    int** cropped = new int*[28];
+    for(int i = 0; i < 28; i++){
+        int* line = new int[23];
+        for(int j = 0; j < 23; j++){
+            //line[j] = (matrix[2*i][2*j] + matrix[2*i][2*j+1] + matrix[2*i+1][j] + matrix[2*i+1][2*j+1])/4;
+            line[j] = 0;
+            for (int k = 0; k < 4; k++){
+                for (int l = 0; l < 4; l++)
+                    line[j] += matrix[4*i+k][4*j+l];
+            }
+        }
+        cropped[i] = line;
+    }
+    return cropped;
+}
+
+int calc_difference_compress(int** m1, int** m2){
+    int res = 0;
+    for(int i = 0; i < 28; i++){
+        for (int j = 0; j < 23; j++){
+            res += abs(m1[i][j]-m2[i][j]);
+        }
+    }
+    return res;
+}
+
+int* get_result_compress(int*** matrix_base, int n, int s, int people){
+    int** cur = compress(picture_to_matrix_int(s, n));
+    int* result = new int[people];
+    for(int i = 0; i < people; i++){
+        result[i] = calc_difference_compress(matrix_base[i], cur);
+    }
+    for(int i = 0; i < 28; i++)
+        delete [] cur[i];
+    delete [] cur;
+    return result;
+}
---
+++ visualization/header.h
@@ -0,0 +1,194 @@
+#ifndef FACE_REC_DOUBLE_HEADER_H
+#define FACE_REC_DOUBLE_HEADER_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+
+
+using namespace sf;
+using namespace std;
+
+/// Каждый метод имеет сходные фунуции
+/// make_base - создание "базы" по эталонам (приписка multi подчеркивает наличие двух эталонов)
+/// get_result - сравнение тестового изображения с каждым в базе,
+/// и получение вектора состоящего из "отличий" от каждого эталона
+
+
+/// check_ans - выбор минимального "отличия" в качестве "ответа"
+/// calculate_difference - сумма модулей покомпонентных разностей двух векторов
+/// get_acc - получение точности
+
+
+
+
+//// Common
+
+int** picture_to_matrix_int(int s, int n);    ///  int matrix -> long matrix
+long ** picture_to_matrix_long(int s, int n); /// .pgm to matrix
+template <typename T>
+T calculate_difference(T* first, T* second, int len) {
+    T diff = 0;
+    for(int i = 0; i < len; i++){
+        diff += abs(first[i] - second[i]);
+    }
+    return diff;
+}
+
+template <typename T>
+void delete_matrix(T** matrix) {
+    for(int i = 0; i < 112; ++i) {
+        delete [] matrix[i];
+    }
+    delete [] matrix;
+} /// Удаление матрицы (освобождение памяти)
+
+template <typename T>
+int check_ans_int(const T *res, int people){
+    T min = res[0];
+    int index = 1;
+    for(int i = 1; i < people; i++){
+        if (min > res[i]) {
+            min = res[i];
+            index = i + 1;
+        }
+    }
+    return index;
+}
+
+float get_acc(int mistakes, int people, int pb); /// pb - количество эталонов
+wstring get_acc(int count, int mistakes);
+
+/// Hist            /// prec - параметр определящиюй количество групп, prec = 256 / {количество групп}
+long * return_hist_v2(long **picture, int prec); /// Гистограмма из матрицы (улучшенный метод)
+
+long* get_result_hist(long** base, int n, int s, int people, int prec);
+
+long ** make_multibase_hist(int people, int prec, int n1, int n2); /// n1, n2 номера изображений - эталонов
+
+/// Pixels
+
+/// Pixels       /// nop (number of pixels) - количество случайных пикселей
+pair<int, int>* choose_pixels(int num); /// Выбор случайных пикселей
+int* extract_vector_of_chosen_pixels(int** matrix, pair<int, int>* pixels, int len); /// Вектор по выбранным пикселям
+int** make_multibase_pixels(int people, pair<int, int>*pixels, int nop, int n1, int n2);
+int* get_result_pixels (int** base, pair<int,int>*pixels, int people, int nop, int s, int n);
+
+/// Compress
+
+int** compress(int** matrix); /// Сжатие матрицы х16
+int*** make_multibase_compress(int people, int n1, int n2);
+int calc_difference_compress(int** m1, int** m2);
+int* get_result_compress(int*** matrix_base, int n, int s, int people);
+
+
+
+/// Display
+struct Screen /// Структура из отображаемых объектов для удостбва визуализации
+        {
+    /// Sprite - по сути, картинка для отображения
+    Sprite test;
+    Text test_text;
+    Sprite hist;
+    Text hist_text;
+    Sprite pixels;
+    Text pixels_text;
+    Sprite compress;
+    Text compress_text;
+    Sprite vote;
+    Text vote_text;
+    Font font;
+    void update_window(RenderWindow &window){
+        test.setPosition(Vector2f(0.0f +17.0f, 0.0f));
+        hist.setPosition(Vector2f(350.0f +17.0f, 0.0f));
+        pixels.setPosition(Vector2f(700.0f +17.0f, 0.0f));
+        compress.setPosition(Vector2f(152.66f, 450.0f));
+        vote.setPosition(Vector2f(2*152.66f + 3 * 92.0f, 450.0f));
+
+        test_text.setPosition(Vector2f(0.0f, 350.0f));
+        hist_text.setPosition(Vector2f(350.0f, 350.0f));
+        pixels_text.setPosition(Vector2f(700.0f, 350.0f));
+        compress_text.setPosition(Vector2f(152.66f, 800.0f));
+        vote_text.setPosition(Vector2f(2*152.66f + 3 * 92.0f, 800.0f));
+
+        window.draw(test);
+        window.draw(test_text);
+        window.draw(hist);
+        window.draw(hist_text);
+        window.draw(pixels);
+        window.draw(pixels_text);
+        window.draw(compress);
+        window.draw(compress_text);
+        window.draw(vote);
+        window.draw(vote_text);
+    }
+
+    void setTest(const Sprite test) {
+        Screen::test = test;
+    }
+
+    void setHist(const Sprite hist) {
+        Screen::hist = hist;
+    }
+
+    void setPixels(const Sprite pixels) {
+        Screen::pixels = pixels;
+    }
+
+    void setVote(const Sprite vote) {
+        Screen::vote = vote;
+    }
+
+    void setCompress(const Sprite compress) {
+        Screen::compress = compress;
+    }
+
+    void setTestText(const wstring& text) {
+        test_text.setFont(font);
+        test_text.setString(text);
+        test_text.setCharacterSize(27);
+        test_text.setFillColor(Color::Black);
+    }
+
+    void setHistText(const wstring& text) {
+        hist_text.setFont(font);
+        hist_text.setString(text);
+        hist_text.setCharacterSize(27);
+        hist_text.setFillColor(Color::Black);
+    }
+
+    void setPixelsText(const wstring& text) {
+        pixels_text.setFont(font);
+        pixels_text.setString(text);
+        pixels_text.setCharacterSize(27);
+        pixels_text.setFillColor(Color::Black);
+    }
+
+    void setCompressText(const wstring& text) {
+        compress_text.setFont(font);
+        compress_text.setString(text);
+        compress_text.setCharacterSize(27);
+        compress_text.setFillColor(Color::Black);
+    }
+
+    void setVoteText(const wstring& text) {
+        vote_text.setFont(font);
+        vote_text.setString(text);
+        vote_text.setCharacterSize(27);
+        vote_text.setFillColor(Color::Black);
+    }
+
+    Screen(){
+        font.loadFromFile("myfont.ttf");
+    }
+};
+
+Sprite get_sprite(int s, int n); /// Получение спрайта из требуемого изображения
+
+void fill_textures(); /// Заполнить текстурами массив
+#endif //FACE_REC_DOUBLE_HEADER_H
